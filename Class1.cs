44607cca8185cb86b4c5d1d5c25bc17ccfa94f3c
Natlia82 <43696028+Natlia82@users.noreply.github.com>
2019-03-26 13:43:43 +0500

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Class1
+    {
+        /// <summary>
+        ///  метод ищет мин. число из трех
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        static public void Mainl(int a, int b, int c)
+        {
+            int min;
+            Console.WriteLine("a = {0} b = {1} c = {2}", a, b, c);
+            if (a < b)
+            {
+                min = a;
+            }
+            else min = b;
+
+            if (min > c)
+            {
+                min = c;
+            }
+
+            Console.WriteLine("мин:  {0}", min);
+
+
+        }
+
+        /// <summary>
+        /// подсчет кол-ва цифр числа n
+        /// </summary>
+        /// <param name="n"></param>
+
+        static public void Number(string n)
+        {
+            Console.WriteLine(n + " кол-во цифр = " + n.Length);
+
+        }
+
+        /// <summary>
+        /// подсчет суммы всех нечетных положительных 
+        /// </summary>
+        static public int Typing(int str, int summa)
+        {
+            int summa1 = str + summa;
+            //  Console.WriteLine("Сумма = {0}", summa1);
+            return summa;
+        }
+
+        /// <summary>
+        /// проверка логина и пароля
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="summa"></param>
+        /// <returns></returns>
+
+        static public Boolean Log(string log, string pas)
+        {
+            Boolean flag;
+              if (log == "root" & pas == "GeekBrains") 
+          //  if (log == "1" & pas == "1")
+            {
+                flag = true;
+            }
+            else { flag = false; }
+            return flag;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //------1
+            int a = 4;
+            int b = 8;
+            int c = 7;
+
+            Class1.Mainl(a, b, c);
+            //----2
+            int n = 123454;
+            Class1.Number(Convert.ToString(n));
+            //----3
+            int str;
+            int summa = 0;
+            do
+            {
+                Console.WriteLine("Введите число:");
+                str = Convert.ToInt32(Console.ReadLine());
+                if (str > 0 && str % 2 == 1)
+                {
+                    summa = Class1.Typing(str, summa);
+                }
+
+            } while (str != 0);
+
+            Console.WriteLine("Сумма = {0}", summa);
+            //-----4
+            Boolean flag = false;
+            int i = 0;
+
+            do
+            {
+                Console.WriteLine("Введите логин:");
+                string log = Console.ReadLine();
+                Console.WriteLine("Введите пароль:");
+                string pas = Console.ReadLine();
+                flag = Class1.Log(log, pas);
+
+            } while (i < 4 && !flag);
+
+            if (flag)
+            {
+                Console.WriteLine("далее........");
+            }
+
+            //-------5
+            Console.WriteLine("Ваш рост в см.? ");
+            string rost = Console.ReadLine();
+            Console.WriteLine("Ваш вес? ");
+            string ves = Console.ReadLine();
+            Console.WriteLine("индекс массы тела: ");
+            double ind = Convert.ToDouble(ves) / (Convert.ToDouble(rost) * Convert.ToDouble(rost));
+
+            Console.WriteLine(Convert.ToString(ind));
+            if (ind < 18)
+            {
+                Console.WriteLine("Ваш индекс: " + Convert.ToString(ind) + " это ниже нормы");
+            }
+            else
+            {
+                if (ind > 25)
+                {
+                    Console.WriteLine("Ваш индекс: " + Convert.ToString(ind) + " это выше нормы");
+                }
+
+                else Console.WriteLine("Ваш индекс: " + Convert.ToString(ind) + " это норма");
+
+            }
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
